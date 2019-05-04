1b84cc6c0ad362d1b7a2483c3938bdfe318c4ce6
smnthreeds <smnthreeds@gmail.com>
2019-05-04 16:15:12 +0100

    Updates

---
+++ precache-manifest.c2ce6a5a59825a8dc8f308a0c9ea25a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3c02cc1dcc83a9ad930",
+    "url": "/cryptotable/static/js/runtime~main.5ec99118.js"
+  },
+  {
+    "revision": "368101e985c32ddf20e3",
+    "url": "/cryptotable/static/js/main.bba5878e.chunk.js"
+  },
+  {
+    "revision": "e28fcb4d7eb4e9310dab",
+    "url": "/cryptotable/static/js/2.f6e0817f.chunk.js"
+  },
+  {
+    "revision": "368101e985c32ddf20e3",
+    "url": "/cryptotable/static/css/main.38a53234.chunk.css"
+  },
+  {
+    "revision": "e28fcb4d7eb4e9310dab",
+    "url": "/cryptotable/static/css/2.198ddd6e.chunk.css"
+  },
+  {
+    "revision": "17d020c1fb2561f36ec99dcb674f459a",
+    "url": "/cryptotable/index.html"
+  }
+];
